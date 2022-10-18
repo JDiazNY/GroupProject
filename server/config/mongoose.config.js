e5c3282038365ead0ec5175091145775d3f3f018
JDiazNY <103845225+JDiazNY@users.noreply.github.com>
2022-10-18 08:45:31 -0400

    Delete server directory

--- server/config/mongoose.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect("mongodb://localhost/ItemListDB")
-.then(() =>{
-    console.log("Successfully Connected to DB")
-})
-.catch(() =>{
-    console.log("Connection Error")
-})
